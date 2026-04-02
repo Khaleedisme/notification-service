57c251da9cd811489382ed091f28d7e40829b164
Khaleedisme <brummerh7@gmail.com>
2026-04-03 01:24:44 +0400

    Email inteqrasiyası əlavə edildi

--- src/main/java/org/example/consumer/EmailConsumer.java
+++ src/main/java/org/example/consumer/EmailConsumer.java
@@ -1,17 +1,34 @@
 package org.example.consumer;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.model.NotificationMessage;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class EmailConsumer {
 
+    private final JavaMailSender mailSender;
+
     @RabbitListener(queues = "${rabbitmq.queue.email}")
     public void consume(NotificationMessage notificationMessage){
-        log.info("Email göndərilir: {}: {}", notificationMessage.getRecipient(), notificationMessage.getContent());
+        try{
+            SimpleMailMessage email = new SimpleMailMessage();
+            email.setTo(notificationMessage.getRecipient());
+            email.setSubject(notificationMessage.getSubject());
+            email.setText(notificationMessage.getContent());
+            mailSender.send(email);
+            log.info("Email göndərilir: {}: {}", notificationMessage.getRecipient(), notificationMessage.getContent());
+        } catch (Exception e){
+            log.error("Email göndərilmədi: {}", e.getMessage());
+            throw new AmqpRejectAndDontRequeueException("Email uğursuz ", e);
+        }
     }
 }
--- src/main/java/org/example/consumer/SmsConsumer.java
+++ src/main/java/org/example/consumer/SmsConsumer.java
@@ -1,16 +1,35 @@
 package org.example.consumer;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 import lombok.extern.slf4j.Slf4j;
 import org.example.model.NotificationMessage;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class SmsConsumer {
 
+    @Value("${twilio.account.sid}")
+    private String accountSid;
+
+    @Value("${twilio.auth.token}")
+    private String authToken;
+
+    @Value("${twilio.phone.number}")
+    private String fromNumber;
+
     @RabbitListener(queues = "${rabbitmq.queue.sms}")
     public void consume(NotificationMessage notificationMessage){
+        Twilio.init(accountSid, authToken);
+        Message.creator(
+                new PhoneNumber(notificationMessage.getRecipient()),
+                new PhoneNumber(fromNumber),
+                notificationMessage.getContent()
+        ).create();
         log.info("SMS göndərilir: {} -> {}",
                 notificationMessage.getRecipient(),
                 notificationMessage.getContent());
